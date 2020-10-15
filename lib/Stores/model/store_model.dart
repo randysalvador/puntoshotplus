e91231dc438cfeb1880761b4524428ea9469f915
Jairo <olan.mayo05@gmail.com>
2020-10-14 20:26:35 -0500

    add changes today

--- lib/Stores/model/store_model.dart
+++ lib/Stores/model/store_model.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 
 class StoreModel {
   int id;
@@ -16,6 +17,30 @@ StoreModel storeModelfromJson(String jsonData) {
   final data = json.decode(jsonData);
   return StoreModel.fromStoreStringJson(data);
 }
+
+class StoresList {
+  final List<StoreModel> data;
+
+  StoresList({this.data});
+
+  factory StoresList.fromJson(List<dynamic> parsedJson) {
+    List<StoreModel> stores = new List<StoreModel>();
+
+    return new StoresList(
+      data: stores,
+    );
+  }
+}
+
+/* class StoreList {
+  final List<StoreModel> data;
+  Store({this.data});
+
+  factory Store.fromArrayJson(Map<String, dynamic> json) {
+    var list = json as List;
+    List<StoreModel> listStore = list.map((e) => StoreModel.fromStoreStringJson(map))
+  }
+} */
 
 // String values = "{ \" numberPhone \": \" +22565786589\", \"country\":\"CI\"}";
 // print(json.decode(values));

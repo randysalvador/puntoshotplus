a2080f1c14011dc8d35ef79df0ae6d3a2ebd4d03
Henry <henry.7mendezz@gmail.com>
2020-10-19 11:09:50 -0500

    add_encrypt_to_membership_controller

--- lib/Membership/membership_controller.dart
+++ lib/Membership/membership_controller.dart
@@ -2,41 +2,42 @@ import 'package:get/get.dart';
 import 'dart:async';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:puntoshots/Membership/membership_model.dart';
+import 'package:encrypt/encrypt.dart';
 
 class QrScanController extends GetxController {
+  String result = "";
+  String barcodeScanRes;
+  QrScanModel membership;
+
   String localStorage =
       "{\"vencimiento\": \"02/01/2050\", \"suscriptor\": \"Rass\", \"membresía\": \"150994\"}";
-  // static QrScanController to = Get.find();
+
   @override
   void onInit() {
     super.onInit();
     print("same as initState");
-    getMemershipByQr(localStorage);
+    getMembershipByQr(localStorage);
   }
 
-  String barcodeScanRes;
-  QrScanModel membership;
+  void decrypt(String barcodeScanRes) {
+    final key = Key.fromUtf8('key'); //'
+    final iv = IV.fromLength(16);
+    final encrypting = Encrypter(AES(key));
+    final decrypted = encrypting.decrypt(barcodeScanRes as Encrypted, iv: iv);
+    result = decrypted;
+    getMembershipByQr(result);
+  }
 
   Future<void> scan() async {
     barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
         "#0058A3", "Salir", true, ScanMode.QR);
-    getMemershipByQr(barcodeScanRes);
+    decrypt(barcodeScanRes);
+  }
+
+  Future<void> getMembershipByQr(String data) async {
+    (data == '')
+        ? print("Error to get QR data")
+        : membership = qrScanfromJson(data);
     update();
   }
-
-  Future<void> getMemershipByQr(String data) async {
-    print("----------------->>> DATA recibida: $data");
-    if (data == '') {
-      print("Error en el sistema xd"); //Widget Mensaje error
-    } else {
-      membership = qrScanfromJson(data);
-      print(membership.membresia);
-      print(membership.suscriptor);
-      print(membership.vencimiento);
-    }
-  }
-
-  //print(barcodeScanRes);
-  //getMemershipByQr();
-
 }
